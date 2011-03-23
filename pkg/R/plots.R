387eb2984db069fad251441af6d14cc292316a9f
jmurray1022 (Jared Murray) <jared.murray@stat.duke.edu>
2011-03-23 02:31:34 +0000

    Fix documentation & use model object
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sbfac@12 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/plots.R
+++ pkg/R/plots.R
@@ -1,19 +1,28 @@
-#' @usage
-#' plotLoadingsHeat <- function(RscApost, xlabel=NA, ylabel=NA, 
+#' Display a heatmap of factor loadings (note that this function requires ggplot2 package to be
+#' installed)
+#' @aliases plotLoadingsHeat
+#' @param model an sbfac model object
+#' @param xlabel x-axis label
+#' @param ylabel y-axis label
+#' @param color character vector of colors
+#' @param sorting a permutaion of 1:P (where P is the number of variables) providing sort order
+#' for the rows of the loadings matrix
+#' @usage plotLoadingsHeat(model, xlabel=NA, ylabel=NA, 
 #' color=c("#053061", "#2166AC", "#4393C3", "#92C5DE", "#D1E5F0", "#F7F7F7", 
-#'      "#FDDBC7", "#F4A582", "#D6604D", "#B2182B", "#67001F"), 
-#' sorting=NA)
-plotLoadingsHeat <- function(RscApost, xlabel=NA, ylabel=NA, 
+#'      "#FDDBC7", "#F4A582", "#D6604D", "#B2182B", "#67001F"), sorting=NA)
+plotLoadingsHeat <- function(model, xlabel=NA, ylabel=NA, 
                     color=c("#053061", "#2166AC", "#4393C3", "#92C5DE", "#D1E5F0", "#F7F7F7", 
                           "#FDDBC7", "#F4A582", "#D6604D", "#B2182B", "#67001F"), 
                     sorting=NA) {
   if (suppressWarnings(!require(ggplot2, quietly=TRUE))) {
     stop("plotLoadingsHeat requires the ggplot2 package (install.packages('ggplot2'))")
   } else {
+    loadings = model$post.loadings.mean
+    rownames(loadings) = model$varlabel
     #ldf$Group = factor(ldf$Group, levels=rownames(scApl)[order(scApl[,1])])
-  	ldf = melt(RscApost)
+  	ldf = melt(loadings)
   	colnames(ldf) = c("Group", "Factor", "value")
-    if (!any(is.na(sorting))) ldf$Group = factor(ldf$Group, levels=rownames(RscApost)[sorting])
+    if (!any(is.na(sorting))) ldf$Group = factor(ldf$Group, levels=rownames(loadings)[sorting])
     ldf$Factor = as.factor(ldf$Factor)
   	breaks = seq(-1,1,by = 0.2)
   	cl = colorRampPalette(color)(21)
@@ -26,9 +35,9 @@ plotLoadingsHeat <- function(RscApost, xlabel=NA, ylabel=NA,
   }
 }
 
-# plotLoadingsTxt <- function(RscApost) {
-# 	ldf = melt(RscApost)
-# 	ldf = cbind(ldf, as.numeric(ldf$value>0), as.factor(1:dim(RscApost)[1]))
+# plotLoadingsTxt <- function(loadings) {
+# 	ldf = melt(loadings)
+# 	ldf = cbind(ldf, as.numeric(ldf$value>0), as.factor(1:dim(loadings)[1]))
 # 	colnames(ldf) = c("Group", "Factor", "value", "sign", "y")
 # 	ldf$Factor = as.factor(ldf$Factor)
 # 	ldf$value = abs(ldf$value)
